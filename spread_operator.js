ce4e61f11ac73c71288f4e5d498186b1a2d15b40
Usenmfon <usenmfonuko@gmail.com>
2026-08-11 13:58:44 +0100

    update: assignments

--- spread_operator.js
+++ spread_operator.js
@@ -1,8 +1,13 @@
 function rand(){
-    return Math.floor(Math.random() * 50)
+    return Math.floor(Math.random() * 5)
 }
 
 let nums = [rand(), rand(), rand(), rand()]
+
+// Kini - 1st rand Handling user interactions (click, hover, submit)
+// Joel - 2nd rand Event listeners
+// Akanimo - 3rd rand Introduction to events
+// Bla - 4th rand Event propagation and bubbling
 
 console.log(nums);
 
